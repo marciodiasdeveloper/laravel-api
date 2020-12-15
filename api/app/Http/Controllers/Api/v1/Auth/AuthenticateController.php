80415dd14d1d6aa0b7c8984ed20bd4fec4eaeb87
Your Name <you@example.com>
2020-12-15 00:26:48 -0200

    Authenticate

--- api/app/Http/Controllers/Api/v1/Auth/AuthenticateController.php
+++ api/app/Http/Controllers/Api/v1/Auth/AuthenticateController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Api\v1\Auth;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Api\v1\Auth\AuthenticateRequest;
+
 
 class AuthenticateController extends Controller
 {
-    //
+    public function store(AuthenticateRequest $request)
+    {
+        $data = $input->validated();
+    }
 }
